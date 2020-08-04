d02949ac6bb87b0c51f6c7884e5454b6b0958c04
otherdcn <danny.c.ntwari@gmail.com>
2020-08-04 16:25:10 +0200

    Test JavaScript functionality with system test
    Also add id to select component for order Pay type for system test to
    work.

--- app/javascript/PayTypeSelector/index.jsx
+++ app/javascript/PayTypeSelector/index.jsx
@@ -33,8 +33,8 @@ class PayTypeSelector extends React.Component {
 		return (
 			<div>
 				<div className="field">
-					<label htmlFor="order_pay_type">Pay Type</label>
-					<select onChange={this.onPayTypeSelected} name="order[pay_type]">
+					<label htmlFor="order_pay_type">Pay type</label>
+					<select id="order_pay_type" onChange={this.onPayTypeSelected} name="order[pay_type]">
 						<option value="">Select a payment method</option>
 						<option value="Check">Check</option>
 						<option value="Credit card">Credit Card</option>
